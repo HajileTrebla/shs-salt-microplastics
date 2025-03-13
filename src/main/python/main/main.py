a3e6662df16a1673a430b15eb530860e2aceaa2f
Eli <eli.quinio17@gmail.com>
2025-03-14 06:09:47 +0800

    Add dir setup and image capture

--- src/main/python/main/main.py
+++ src/main/python/main/main.py
@@ -5,6 +5,10 @@ import cv2
 from ultralytics import YOLO
 
 
+def init_setup():
+    global root_path
+    os.makedirs(os.path.join(root_path, 'saved_images'), exist_ok=True)
+
 def onMouse(event, x, y, flags, param):
     global clicked
     if event == cv2.EVENT_LBUTTONDOWN:
@@ -18,10 +22,25 @@ def shutdown():
     else:
         os.system('sudo shutdown now')
 
+def capture(data):
+    global root_path
+
+    save_dir = os.path.join(root_path, 'saved_images')
+
+    frame, count, text_w, text_h = data
+    path_time = datetime.now().strftime("%d%m%y.%H%M%S")
+
+    file_name = os.path.join(save_dir,str(path_time)+'.jpeg')
+
+    cv2.rectangle(frame, (0,65), ( text_w, 0-(text_h*2)), (0,0,0), -1)
+    cv2.putText(frame, "Microplastics Detected :"+str(count), (20,20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
+    cv2.putText(frame, "Time :"+str(datetime.now().strftime("%d/%m/%y %H:%M:%S")), (20,60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
+
+    cv2.imwrite(file_name, frame)
+    print('Captured Image : {}'.format(file_name))
 
 def stream(cap, model):
     plastics = 0
-    curr_time = datetime.now()
 
     success, frame = cap.read()
 
@@ -36,29 +55,33 @@ def stream(cap, model):
         text_size, _ = cv2.getTextSize("Microplastics Detected :"+str(plastics), cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
         text_w, text_h = text_size
 
-        cv2.rectangle(annotated_frame, (0,65), ( w, 0-(text_h*2)), (0,0,0), -1)
+        cv2.rectangle(annotated_frame, (0,25), ( w, 0-(text_h*2)), (0,0,0), -1)
         cv2.putText(annotated_frame, "Microplastics Detected :"+str(plastics), (20,20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
-        cv2.putText(annotated_frame, "Time :"+str(curr_time), (20,60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
         cv2.imshow("Microscope", annotated_frame)
+    return annotated_frame, plastics, w, text_h
 
 def detect():
     global clicked
+    global power_button
+    global root_path
 
-    root_path = dirname(dirname(dirname(dirname(dirname(__file__)))))
     model_path = join(root_path, 'saltplas.pt')
 
     model = YOLO(model_path)
 
-
     cap = cv2.VideoCapture(0)
     cv2.namedWindow("Microscope", cv2.WINDOW_NORMAL)
-    cv2.setWindowProperty("Microscope", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    # cv2.setWindowProperty("Microscope", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     cv2.setMouseCallback('Microscope', onMouse)
 
     print('Showing camera feed. Click window or press any key to stop.')
 
-    while cv2.waitKey(1) == -1 and not clicked:
-        stream(cap, model)
+    while not power_button:
+        while cv2.waitKey(1) == -1 and not clicked:
+            stream(cap, model)
+            time.sleep(1)
+        capture(stream(cap, model))
+        clicked = False
 
     cv2.destroyAllWindows()
     time.sleep(1)
@@ -66,8 +89,14 @@ def detect():
 
 def main():
     global clicked
+    global power_button
+    global root_path
 
+    root_path = dirname(dirname(dirname(dirname(dirname(__file__)))))
     clicked = False
+    power_button = False
+
+    init_setup()
     detect()
 
 if __name__ == "__main__":
